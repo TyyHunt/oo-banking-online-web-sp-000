20cc7dc015bd14370a18f2a6392fed7216bccd86
Tyler Hunt <tyhunt04@gmail.com>
2019-02-27 16:49:51 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,6 +1,6 @@
 class Transfer
 
-  attr_accessor :sender, :receiver, :amount
+  attr_accessor :sender, :receiver, :amount, :status
 
   def initialize(sender, receiver, amount)
     @sender = sender
@@ -9,5 +9,7 @@ class Transfer
     @amount = amount
   end
 
+  def valid?
+  end
 
 end
